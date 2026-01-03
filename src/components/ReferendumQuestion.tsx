8646b73302b5ea9109ff24d1318dca74533966a6
Your Namemailtohamim <mailtohamim@gmail.com>
2026-01-03 11:55:36 +0600

    fix: Add missing AnimatePresence import to ReferendumQuestion

--- src/components/ReferendumQuestion.tsx
+++ src/components/ReferendumQuestion.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { useState } from "react";
 import ShinyCard from "./ShinyCard";
 
